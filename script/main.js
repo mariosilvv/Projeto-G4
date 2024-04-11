55cc2a69aff57e4edb3785acbf0e9eced58b2d28
Daniel Harrison <d.harrison.lima@gmail.com>
2024-04-11 15:54:19 -0300

    correções

--- script/main.js
+++ script/main.js
@@ -1,7 +1,5 @@
-import { initSlide } from "./modules/slide.js";
 import { initMenuMobile } from "./modules/menuMobile.js";
 import { initAtalhos } from "./modules/atalhos.js";
 
-initSlide();
 initMenuMobile();
 initAtalhos();
